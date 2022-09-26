a66c1320f063ff55403faf2a464e3af24f216839
amos chan <amoschan1@gmail.com>
2022-09-26 23:35:59 +0800

    change

--- app.js
+++ app.js
@@ -185,8 +185,8 @@ app.listen(port, function() {
 
 
 //test on local db
-app.listen(3000, function() {                                                   //start server on port 3000
-  console.log("Server started on port 3000");
-});
+// app.listen(3000, function() {                                                   //start server on port 3000
+//   console.log("Server started on port 3000");
+// });
 
 
